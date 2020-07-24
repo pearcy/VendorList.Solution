aea09c98ab09d259b556ccab2f55faa2e7f8814a
Joseph Pearce <joseph.pearce05@gmail.com>
2020-07-24 12:11:21 -0700

    troubleshoot routing error

--- VendorList/Controllers/HomeController.cs
+++ VendorList/Controllers/HomeController.cs
@@ -6,14 +6,25 @@ namespace VendorList.Controllers
   public class HomeController : Controller
   {
 
-    [Route("/")]
+    [HttpGet("/")]
     public ActionResult Index()
     {
-      Order starterItem = new Order("Add first order list");
-      return View(starterItem);
+      Order starterOrder = new Order("Add first order list");
+      return View(starterOrder);
     }
 
+     [HttpGet("/orders")]
+    public ActionResult New()
+    {
+      return View();
+    }
 
+     [HttpGet("/orders")]
+    public ActionResult Create(string title)
+    {
+      Order myOrder = new Order(title);
+      return View("Index", myOrder);
+    }
 
 
   }
