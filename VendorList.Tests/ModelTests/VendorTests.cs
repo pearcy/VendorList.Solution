8c7b378db09fcea2e06f2bd3277ccf4ea235de96
Joseph Pearce <joseph.pearce05@gmail.com>
2020-07-24 14:18:52 -0700

    meaningful fail - retrieve Vendor objects  objects to display in our app

--- VendorList.Tests/ModelTests/VendorTests.cs
+++ VendorList.Tests/ModelTests/VendorTests.cs
@@ -42,6 +42,23 @@ namespace VendorList.Tests
       Assert.AreEqual(1, result);
     }
 
+     [TestMethod]
+  public void GetAll_ReturnsAllVendorObjects_VendorList()
+  {
+    //Arrange
+    string name01 = "Daisy Cafe";
+    string name02 = "Rockies Muffins";
+    Vendor newVendor1 = new Vendor(name01);
+    Vendor newVendor2 = new Vendor(name02);
+    List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+
+    //Act
+    List<Vendor> result = Vendor.GetAll();
+
+    //Assert
+    CollectionAssert.AreEqual(newList, result);
+  }
+
     
 
   }
